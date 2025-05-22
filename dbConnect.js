29342e229cfb308d2c1de5ec6f991fa844b28179
Tom Bernard <tebernard@ncu0206944.fda.gov>
2025-05-22 13:22:46 -0500

    Making queries to postgres and returning values into Datatables on the client side

--- dbConnect.js
+++ dbConnect.js
@@ -1,14 +1,7 @@
-// const { Client } = require('pg');
-//import { defineArguments } from "graphql/type/definition";
-//import Client from "pg";
-//const { Client } = require('pg');
 import pg from 'pg';
 const Client = pg.Client;
 
-
-// const dbconnect = async (parm) => {
 export function dbconnect(parm) {
-  // module.exports = function dbconnect(){
   console.log("parm :", parm);
   const your_port = 5432;
   console.log("your_port :", your_port);
@@ -30,16 +23,18 @@ export function dbconnect(parm) {
       console.error("Error connecting to PostgreSQL database:", err);
     });
 
-  //   client.query('SELECT NOW()')
-  //   .then(res => {
-  //     console.log(res.rows[0]);
-  //   })
-  //   .catch(err => {
-  //     console.error('Error executing query:', err);
-  //   })
-  //   .finally(() => {
-  //     client.end(); // Close the connection
-  //   });
-}
+    console.log('from dbconnect parm :', parm);
 
-//export default dbconnect;
+    client.query(parm)
+    .then(res => {
+      // console.log(res.rows[0]);
+      console.log(res.rows);
+      return Promise.resolve(res.rows[0]);
+    })
+    .catch(err => {
+      console.error('Error executing query:', err);
+    })
+    .finally(() => {
+      client.end(); // Close the connection
+    });
+}
--- server.js
+++ server.js
@@ -1,20 +1,42 @@
 const express = require("express");
-const dbconnect = require('./dbConnect.js');
+//const dbconnect = require("./dbConnect.js");
 //var dbconnect = require('./dbConnect.js');
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3080;
-//import dbconnect from './dbConnect.js'
 
-//dbconnect("test");
- dbconnect.dbconnect('test');
+const pg = require("pg");
+const Client = pg.Client;
 
-//dbconnect.dbconnect("Yeahbud");
+const db_port = 5432;
+const client = new Client({
+  user: "tom",
+  host: "ncu0206944",
+  database: "testdb",
+  password: "yeahbuddy",
+  port: db_port, // default is 5432
+});
+client.connect();
+
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+app.get("/cars", async (req, res) => {
+  try {
+
+    const query = "SELECT * from cars;";
+    const qryresult = await client.query(query);
+    //res.status(200).json(qryresult.rows)
+    res.status(200).json(qryresult.rows)
+  } catch (err) {
+    console.error(err);
+    console.error(err.body);
+    res.status(500).send("Server error");
+  }
 });
 
 app.listen(port, () => {
